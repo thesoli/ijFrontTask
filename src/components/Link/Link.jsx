82ac289c90ecf997fac16bf9a44f2ea1f876db05
Soli <137307011+thesoli@users.noreply.github.com>
2024-09-22 18:52:56 +0330

    Add files via upload

---
+++ src/components/Link/Link.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Link.css";
+
+const Link = () => {
+  return (
+    <div className="link">
+      <a href="https://www.0100pooshak.com/" class="link" target="_blank">
+        0100pooshak.com
+      </a>
+    </div>
+  );
+};
+
+export default Link;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Title from "../Title/title";
+import Link from "../Link/Link";
+import Footer from "../Footer/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <Title />
+      <Link />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
